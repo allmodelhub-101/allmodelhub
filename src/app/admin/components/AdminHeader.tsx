77a61e65171cb2602e834e11e4abcd5f72ca024d
allmodelhub-101 <allmodelhub@gmail.com>
2026-09-03 19:51:38 +0500

    Add files via upload

---
+++ src/app/admin/components/AdminHeader.tsx
@@ -0,0 +1,41 @@
+export default function AdminHeader(){
+
+return (
+
+<header
+
+className="
+h-20
+border-b
+flex
+items-center
+justify-between
+px-8
+bg-white
+dark:bg-black
+dark:text-white
+"
+
+>
+
+
+<h1 className="font-bold text-xl">
+
+Admin Control Center
+
+</h1>
+
+
+<div>
+
+All Model Hub
+
+</div>
+
+
+</header>
+
+
+)
+
+}
