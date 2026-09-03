d7b9c32e37c51c33ced6efbef06a6ba90a31b6cb
allmodelhub-101 <allmodelhub@gmail.com>
2026-09-03 19:59:06 +0500

    Create page.tsx

---
+++ src/app/admin/settings/page.tsx
@@ -0,0 +1,41 @@
+export default function SettingsPage(){
+
+return (
+
+<div className="p-8">
+
+
+<h1
+className="
+text-3xl
+font-bold
+mb-6
+"
+>
+System Settings
+</h1>
+
+
+<div
+className="
+rounded-2xl
+border
+p-6
+bg-white
+dark:bg-black
+"
+>
+
+<p>
+Platform configuration and admin controls will appear here.
+</p>
+
+
+</div>
+
+
+</div>
+
+)
+
+}
