13d91130ad877f658c49a8819c3e0f5a4a8d0d78
allmodelhub-101 <allmodelhub@gmail.com>
2026-09-03 19:58:21 +0500

    Create page.tsx

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,41 @@
+export default function UsersPage(){
+
+return (
+
+<div className="p-8">
+
+
+<h1
+className="
+text-3xl
+font-bold
+mb-6
+"
+>
+User Management
+</h1>
+
+
+<div
+className="
+rounded-2xl
+border
+p-6
+bg-white
+dark:bg-black
+"
+>
+
+<p>
+User analytics, accounts and activity management will appear here.
+</p>
+
+
+</div>
+
+
+</div>
+
+)
+
+}
