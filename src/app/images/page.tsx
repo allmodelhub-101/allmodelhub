622dd7b4713b36089173b5ecf6492b51b3f4e090
allmodelhub-101 <allmodelhub@gmail.com>
2026-09-11 16:56:38 +0500

    Add safe asset handoff: src/app/images/page.tsx

--- src/app/images/page.tsx
+++ src/app/images/page.tsx
@@ -1,9 +1,11 @@
 import { AppShell } from "@/components/app-shell";
 import { ImageStudio } from "@/components/image-studio";
+import { isFeatureEnabled } from "@/lib/feature-flags";
 
 export const dynamic = "force-dynamic";
 
-export default function Page() {
-  return <AppShell><ImageStudio /></AppShell>;
+export default async function Page() {
+  const crossModalityHandoffs=await isFeatureEnabled("cross_modality_handoffs");
+  return <AppShell><ImageStudio crossModalityHandoffs={crossModalityHandoffs} /></AppShell>;
 }
 
